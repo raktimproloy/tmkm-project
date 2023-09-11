b944d21dc8139a975ca3bc894e8ba1be82bfe77c
Joshim Uddin <idkhs8211@gmail.com>
2023-09-11 22:33:26 +0600

    solve build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer/>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js" async/>
       </body>
     </html>
   )
